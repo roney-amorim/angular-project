4ef6d7eec485288e8f6b1d127edb557295975b54
roney-amorim <roney.augusto.amorim@gmail.com>
2019-04-18 23:27:02 -0300

    equalização módulo 1

--- src/app/clientes/listar/listar.component.ts
+++ src/app/clientes/listar/listar.component.ts
@@ -8,63 +8,9 @@ import { Observable, observable } from 'rxjs';
 })
 export class ListarComponent implements OnInit {
 
-  valorObservable: string;
-  valorPromise: string;
-
   constructor() { }
 
   ngOnInit() {
-    // this.semAssinante();
-    // this.lazyAndEager();
-    // this.multiplosValores();
-  }
-  semAssinante() {
-    let observer: Observable<string> = new Observable<string>(observable => {
-      this.valorObservable = 'Instanciando um Observable';
-    });
-    let promise: Promise<string> = new Promise<string>(resolve => {
-      this.valorPromise = 'Instanciando um Promise';
-    });
-
   }
 
-  lazyAndEager() {
-    let observer: Observable<string> = new Observable<string>((observer) => {
-      observer.next('Instanciando um Observable');
-    });
-
-    let promise: Promise<string> = new Promise<string>((resolve, reject) => {
-      resolve('Instanciando um Promise');
-    });
-
-    observer.subscribe(observable => {
-      this.valorObservable = observable;
-    });
-
-    promise.then(resolve => {
-      this.valorPromise = resolve;
-    });
-  }
-
-  multiplosValores() {
-    let observer: Observable<string> = new Observable<string>((observer) => {
-      observer.next('Instanciando um Observable');
-      observer.next('Instanciando um outro Observable');
-    });
-
-    let promise: Promise<string> = new Promise<string>((resolve, reject) => {
-      resolve('Instanciando um Promise');
-      resolve('Instanciando um outro Promise');
-    });
-
-    observer.subscribe(observable => {
-      console.log(observable);
-      this.valorObservable = observable;
-    });
-
-    promise.then(resolve => {
-      console.log(resolve);
-      this.valorPromise = resolve;
-    });
-  }
 }
